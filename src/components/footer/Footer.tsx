1e61c65fe3a774bfbed5c3a41bb81eaaa5dd44de
YuraSS23 <nikitsenko_yura@mail.ru>
2024-01-26 14:45:06 +0300

    1920 end version + 360 end version

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import './Footer.css';
-import footerImg from "../../assets/images/Vector 23.svg"
 
 export const Footer = () => {
     return <footer>
-        {/*<div className={"triangle"}></div>*/}
-  {/*      <img src={footerImg}/>*/}
     </footer>
 };
