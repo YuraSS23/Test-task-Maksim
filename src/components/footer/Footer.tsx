ba121a31bc00588dd994b41ee21c24c8c648b54e
YuraSS23 <nikitsenko_yura@mail.ru>
2024-01-26 11:56:07 +0300

    1920 end version + 360 end version

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import footerImg from "../../assets/images/Vector 23.svg"
 
 export const Footer = () => {
     return <footer>
-        <div className={"footerimg"}></div>
+        {/*<div className={"triangle"}></div>*/}
   {/*      <img src={footerImg}/>*/}
     </footer>
 };
